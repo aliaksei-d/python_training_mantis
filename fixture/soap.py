2b5ff89d32376adee03beb4240c571e4ac7a676b
doborovicha <aliaksei.doborovich@gmail.com>
2020-03-24 20:49:08 +0300

    - task 26: base_url added to soap.py

--- fixture/soap.py
+++ fixture/soap.py
@@ -10,7 +10,7 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.base_url + "/api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
@@ -18,7 +18,7 @@ class SoapHelper:
             return False
 
     def get_link(self):
-        return Client(self.app.config['web']['baseUrl'] + "api/soap/mantisconnect.php?wsdl")
+        return Client(self.app.base_url + "/api/soap/mantisconnect.php?wsdl")
 
     def get_project_list(self, username, password):
         client = self.get_link()
